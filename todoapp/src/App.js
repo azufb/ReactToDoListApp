c67a3c43c55665584bc5cd940a984025cf1f02c4
Azusa Okamoto <curiousazunas@gmail.com>
2020-07-21 13:08:58 +0900

    doSubmit部分の作成

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -3,18 +3,25 @@ import './App.css';
 
 class App extends Component {
   input = '';
-  
+
   // イベントを発生させたオブジェクトへの参照。
   doChange(event) {
     this.input = event.target.value;
   }
+
+  doSubmit(event) {
+    this.setState({
+      newTodo: [this.input]
+    });
+  }
+
   render () {
     return <div>
       <h1>ToDoList</h1>
       <label>
         <input type="text" onChange={this.doChange} />
       </label>
-      <button type="submit">追加</button>
+      <button type="submit" onClick={this.doSubmit}>追加</button>
     </div>
   }
 }
